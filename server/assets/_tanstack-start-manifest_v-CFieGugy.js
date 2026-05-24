77471520c2e9ec6626e57cca465a1a330498eb32
saumya <soul34devils@gmail.com>
2026-05-24 13:01:28 +0530

    Updates

--- server/assets/_tanstack-start-manifest_v-CFieGugy.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Lenovo/OneDrive/Documents/documents/portfoliov2/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/saumya-portfolio/assets/index-ybXDVL8-.js"] }, "/": { filePath: "C:/Users/Lenovo/OneDrive/Documents/documents/portfoliov2/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/saumya-portfolio/assets/index-DsKAsE6S.js"] } }, clientEntry: "/saumya-portfolio/assets/index-ybXDVL8-.js" });
-export {
-  tsrStartManifest
-};
